871e8551642528cc547c9f16c24f96798ac0f763
Maksim Mikhailov <max@mer.pw>
2023-01-31 21:21:19 +0200

    Add simple Board Add constructor to read level from matrix

--- out/dist/js/board.js
+++ out/dist/js/board.js
@@ -1,5 +1,17 @@
-export default class Cell {
-    constructor() {
-        this.type = "empty";
-    }
+export class Cell {
+  constructor(type = "empty") {
+    this.type = "empty"
+    this.type = type
+  }
+}
+export class Board {
+  constructor(boardNums) {
+    this.width = 0
+    this.height = 0
+    this.cells = boardNums.map((row) =>
+      row.map((cell) => new Cell(cell === 0 ? "wall" : "empty"))
+    )
+    this.width = this.cells[0].length
+    this.height = this.cells.length
+  }
 }
--- src/board.ts
+++ src/board.ts
@@ -1,3 +1,21 @@
-export default class Cell {
-  public type: "empty" | "wall" | "start" | "end" = "empty"
+export class Cell {
+  public type: "empty" | "wall" = "empty"
+
+  constructor(type: "empty" | "wall" = "empty") {
+    this.type = type
+  }
+}
+
+export class Board {
+  public cells: Cell[][]
+  public width = 0
+  public height = 0
+
+  constructor(boardNums: number[][]) {
+    this.cells = boardNums.map((row) =>
+      row.map((cell) => new Cell(cell === 0 ? "wall" : "empty"))
+    )
+    this.width = this.cells[0].length
+    this.height = this.cells.length
+  }
 }
