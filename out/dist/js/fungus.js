517829fcc3e753022d9ba2f97d90b0ad3e83a702
healingdrawing <kyznector@gmail.com>
2023-02-09 01:07:50 +0200

    add fungus

---
+++ out/dist/js/fungus.js
@@ -0,0 +1,5 @@
+"use strict";
+// TODO implement Fungus class.
+// Follow the Hero class as an example, with modifications.
+// no move required, but demolution by pressing T keyboard key,
+// and mounting by pressing F keyboard key.
---
+++ src/fungus.ts
@@ -0,0 +1,4 @@
+// TODO implement Fungus class.
+// Follow the Hero class as an example, with modifications.
+// no move required, but demolution by pressing T keyboard key,
+// and mounting by pressing F keyboard key.
