b5d8a752d6dda2ff49f9334bca5757b5df7d3a12
natkim <natalia.kim.masseli@gmail.com>
2023-02-15 18:34:04 +0200

    refactor level sheeps. sheep initialisation moved from "levels.ts" into "board.ts".

--- out/dist/js/board.js
+++ out/dist/js/board.js
@@ -1,3 +1,4 @@
+import Sheep from "./sheep.js";
 export const CELL_SIZE = 5;
 const CELL_TYPES = [
     { type: "empty", color: "white" },
@@ -5,77 +6,91 @@ const CELL_TYPES = [
     { type: "bush", color: "green" },
 ];
 export class Cell {
-    constructor(typeCode) {
+    constructor(typeCode, x, y) {
         this.type = CELL_TYPES[typeCode].type;
         this.element = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        this.x = x;
+        this.y = y;
+        this.element.x.baseVal.value = this.x * CELL_SIZE;
+        this.element.y.baseVal.value = this.y * CELL_SIZE;
         this.element.width.baseVal.value = CELL_SIZE;
         this.element.height.baseVal.value = CELL_SIZE;
         this.element.style.fill = CELL_TYPES[typeCode].color;
     }
 }
 export class Board {
+    // sheeps: Sheep[]
     render(frameTimeDiff) {
         this.hero.render(frameTimeDiff, this.cells);
-        this.bushes.forEach((bush) => bush.render(this.sheeps));
-        // clean up the eaten bushes
-        this.bushes = this.bushes.filter((bush) => !bush.eaten);
-        this.sheeps.forEach((sheep) => sheep.render(frameTimeDiff, this.cells));
+        // this.sheeps.forEach((sheep) => sheep.render(frameTimeDiff, this.cells))
     }
-    getRandomEmptyCell(setType = CELL_TYPES[0].type) {
-        const x = Math.floor(Math.random() * this.cells[0].length);
-        const y = Math.floor(Math.random() * this.cells.length);
-        if (this.cells[y][x].type !== "empty" ||
-            this.usedCellsYX.find((yx) => yx.x === x && yx.y === y)) {
-            return this.getRandomEmptyCell(setType);
+    getNeighbors(cell) {
+        const neighbors = {
+            right: this.cells[cell.y][cell.x + 1],
+            left: this.cells[cell.y][cell.x - 1],
+            bottom: this.cells[cell.y + 1][cell.x],
+            top: this.cells[cell.y - 1][cell.x],
+            bottomRight: this.cells[cell.y + 1][cell.x + 1],
+            topLeft: this.cells[cell.y - 1][cell.x - 1],
+            topRight: this.cells[cell.y - 1][cell.x + 1],
+            bottomLeft: this.cells[cell.y + 1][cell.x - 1],
+        };
+        for (const key in neighbors) {
+            if (!neighbors[key]) {
+                return null;
+            }
         }
-        this.usedCellsYX.push({ y, x });
-        this.cells[y][x].type = setType;
-        return this.cells[y][x];
+        return neighbors;
     }
-    constructor(svg, hero, bushes, sheeps, boardNums) {
-        this.bushes = [];
-        this.sheeps = [];
-        this.usedCellsYX = [];
+    getRandomEmptyCells(count) {
+        const takenCells = new Set();
+        const getRandomEmptyCell = () => {
+            const x = Math.floor(Math.random() * this.cells[0].length);
+            const y = Math.floor(Math.random() * this.cells.length);
+            const cell = this.cells[y][x];
+            if (cell.type === "empty" && !takenCells.has(cell)) {
+                return cell;
+            }
+            return getRandomEmptyCell();
+        };
+        for (let i = 0; i < count; i++) {
+            takenCells.add(getRandomEmptyCell());
+        }
+        return [...takenCells];
+    }
+    constructor(svg, hero, level) {
         this.hero = hero;
-        this.sheeps = sheeps;
-        this.bushes = bushes;
-        boardNums.forEach((row) => {
+        svg.getElementById("players").appendChild(hero.element);
+        const board = level.board;
+        board.forEach((row) => {
             row.push(1);
             row.unshift(1);
         });
-        boardNums.push(new Array(boardNums[0].length).fill(1));
-        boardNums.unshift(new Array(boardNums[0].length).fill(1));
-        this.cells = boardNums.map((row) => row.map((cellCode) => new Cell(cellCode)));
-        // get the group g element from the svg
+        board.push(new Array(board[0].length).fill(1));
+        board.unshift(new Array(board[0].length).fill(1));
         const landscape = svg.querySelector("#landscape");
-        this.cells.forEach((row, y) => {
-            row.forEach((cell, x) => {
-                cell.element.x.baseVal.value = x * CELL_SIZE;
-                cell.element.y.baseVal.value = y * CELL_SIZE;
-                landscape.appendChild(cell.element);
-            });
+        this.cells = board.map((row, y) => row.map((cellCode, x) => {
+            const cell = new Cell(cellCode, x, y);
+            landscape.appendChild(cell.element);
+            return cell;
+        }));
+        const emptyCells = this.getRandomEmptyCells(level.sheepCount + 1);
+        const heroCell = emptyCells[0];
+        const sheeps = emptyCells
+            .slice(1)
+            .map((cell) => new Sheep(cell.x, cell.y, false, 0));
+        const sheepsGroup = svg.querySelector("#sheeps");
+        sheeps.forEach((sheep) => {
+            sheepsGroup.appendChild(sheep.element);
         });
-        const heroCell = this.getRandomEmptyCell();
         this.hero.x =
             heroCell.element.x.baseVal.value + (CELL_SIZE - this.hero.width) / 2;
         this.hero.y =
             heroCell.element.y.baseVal.value + (CELL_SIZE - this.hero.height) / 2;
-        // place sheeps on the board
-        this.sheeps.forEach((sheep) => {
-            const sheepCell = this.getRandomEmptyCell();
-            sheep.x =
-                sheepCell.element.x.baseVal.value + (CELL_SIZE - sheep.width) / 2;
-            sheep.y =
-                sheepCell.element.y.baseVal.value + (CELL_SIZE - sheep.height) / 2;
-        });
-        // place bushes on the board
-        this.bushes.forEach((bush) => {
-            const bushCell = this.getRandomEmptyCell(CELL_TYPES[2].type);
-            bush.x = bushCell.element.x.baseVal.value + (CELL_SIZE - bush.width) / 2;
-            bush.y = bushCell.element.y.baseVal.value + (CELL_SIZE - bush.height) / 2;
-        });
         this.width = this.cells[0].length * CELL_SIZE;
         this.height = this.cells.length * CELL_SIZE;
+        svg.viewBox.baseVal.width = this.width;
+        svg.viewBox.baseVal.height = this.height;
     }
 }
 /*
@@ -95,4 +110,4 @@ function intersectRect(r1: DOMRect, r2: DOMRect, gap = 0) {
     bottom: r2.top - r1.bottom < 0,
   }
 }
-*/ 
+*/
--- src/board.ts
+++ src/board.ts
@@ -1,6 +1,6 @@
-import Bush from "./bush"
 import Hero from "./hero"
-import Sheep from "./sheep"
+import { Level } from "./levels"
+import Sheep from "./sheep.js"
 
 export const CELL_SIZE = 5
 
@@ -13,15 +13,23 @@ const CELL_TYPES = [
 type CellType = (typeof CELL_TYPES)[number]["type"]
 
 export class Cell {
-  public type: CellType
+  type: CellType
   element: SVGRectElement
+  x: number
+  y: number
 
-  constructor(typeCode: number) {
+  constructor(typeCode: number, x: number, y: number) {
     this.type = CELL_TYPES[typeCode].type
     this.element = document.createElementNS(
       "http://www.w3.org/2000/svg",
       "rect"
     )
+    this.x = x
+    this.y = y
+
+    this.element.x.baseVal.value = this.x * CELL_SIZE
+    this.element.y.baseVal.value = this.y * CELL_SIZE
+
     this.element.width.baseVal.value = CELL_SIZE
     this.element.height.baseVal.value = CELL_SIZE
 
@@ -29,101 +37,98 @@ export class Cell {
   }
 }
 
-interface YX {
-  x: number
-  y: number
-}
-
 export class Board {
   public cells: Cell[][]
   public width: number
   public height: number
   public hero: Hero
-  bushes: Bush[] = []
-  sheeps: Sheep[] = []
-  usedCellsYX: YX[] = []
+  // sheeps: Sheep[]
 
   render(frameTimeDiff: number) {
     this.hero.render(frameTimeDiff, this.cells)
-
-    this.bushes.forEach((bush) => bush.render(this.sheeps))
-    // clean up the eaten bushes
-    this.bushes = this.bushes.filter((bush) => !bush.eaten)
-
-    this.sheeps.forEach((sheep) => sheep.render(frameTimeDiff, this.cells))
+    // this.sheeps.forEach((sheep) => sheep.render(frameTimeDiff, this.cells))
   }
 
-  getRandomEmptyCell(setType: CellType = CELL_TYPES[0].type): Cell {
-    const x = Math.floor(Math.random() * this.cells[0].length)
-    const y = Math.floor(Math.random() * this.cells.length)
-    if (
-      this.cells[y][x].type !== "empty" ||
-      this.usedCellsYX.find((yx) => yx.x === x && yx.y === y)
-    ) {
-      return this.getRandomEmptyCell(setType)
+  getNeighbors(cell: Cell) {
+    const neighbors = {
+      right: this.cells[cell.y][cell.x + 1],
+      left: this.cells[cell.y][cell.x - 1],
+      bottom: this.cells[cell.y + 1][cell.x],
+      top: this.cells[cell.y - 1][cell.x],
+      bottomRight: this.cells[cell.y + 1][cell.x + 1],
+      topLeft: this.cells[cell.y - 1][cell.x - 1],
+      topRight: this.cells[cell.y - 1][cell.x + 1],
+      bottomLeft: this.cells[cell.y + 1][cell.x - 1],
     }
-    this.usedCellsYX.push({ y, x })
-    this.cells[y][x].type = setType
-    return this.cells[y][x]
+    for (const key in neighbors) {
+      if (!neighbors[key as keyof typeof neighbors]) {
+        return null
+      }
+    }
+    return neighbors
   }
 
-  constructor(
-    svg: SVGSVGElement,
-    hero: Hero,
-    bushes: Bush[],
-    sheeps: Sheep[],
-    boardNums: number[][]
-  ) {
-    this.hero = hero
-    this.sheeps = sheeps
-    this.bushes = bushes
+  getRandomEmptyCells(count: number): Cell[] {
+    const takenCells: Set<Cell> = new Set()
 
-    boardNums.forEach((row) => {
+    const getRandomEmptyCell = (): Cell => {
+      const x = Math.floor(Math.random() * this.cells[0].length)
+      const y = Math.floor(Math.random() * this.cells.length)
+      const cell = this.cells[y][x]
+      if (cell.type === "empty" && !takenCells.has(cell)) {
+        return cell
+      }
+      return getRandomEmptyCell()
+    }
+    for (let i = 0; i < count; i++) {
+      takenCells.add(getRandomEmptyCell())
+    }
+    return [...takenCells]
+  }
+
+  constructor(svg: SVGSVGElement, hero: Hero, level: Level) {
+    this.hero = hero
+    svg.getElementById("players").appendChild(hero.element)
+
+    const board = level.board
+    board.forEach((row) => {
       row.push(1)
       row.unshift(1)
     })
-    boardNums.push(new Array(boardNums[0].length).fill(1))
-    boardNums.unshift(new Array(boardNums[0].length).fill(1))
+    board.push(new Array(board[0].length).fill(1))
+    board.unshift(new Array(board[0].length).fill(1))
 
-    this.cells = boardNums.map((row) =>
-      row.map((cellCode) => new Cell(cellCode))
+    const landscape = svg.querySelector("#landscape") as SVGGElement
+    this.cells = board.map((row, y) =>
+      row.map((cellCode, x) => {
+        const cell = new Cell(cellCode, x, y)
+        landscape.appendChild(cell.element)
+        return cell
+      })
     )
 
-    // get the group g element from the svg
-    const landscape = svg.querySelector("#landscape") as SVGGElement
+    const emptyCells = this.getRandomEmptyCells(level.sheepCount + 1)
+    const heroCell = emptyCells[0]
 
-    this.cells.forEach((row, y) => {
-      row.forEach((cell, x) => {
-        cell.element.x.baseVal.value = x * CELL_SIZE
-        cell.element.y.baseVal.value = y * CELL_SIZE
-        landscape.appendChild(cell.element)
-      })
+    const sheeps = emptyCells
+      .slice(1)
+      .map((cell) => new Sheep(cell.x, cell.y, false, 0))
+
+    const sheepsGroup = svg.querySelector("#sheeps") as SVGGElement
+    sheeps.forEach((sheep) => {
+      sheepsGroup.appendChild(sheep.element)
     })
 
-    const heroCell = this.getRandomEmptyCell()
     this.hero.x =
       heroCell.element.x.baseVal.value + (CELL_SIZE - this.hero.width) / 2
     this.hero.y =
       heroCell.element.y.baseVal.value + (CELL_SIZE - this.hero.height) / 2
 
-    // place sheeps on the board
-    this.sheeps.forEach((sheep) => {
-      const sheepCell = this.getRandomEmptyCell()
-      sheep.x =
-        sheepCell.element.x.baseVal.value + (CELL_SIZE - sheep.width) / 2
-      sheep.y =
-        sheepCell.element.y.baseVal.value + (CELL_SIZE - sheep.height) / 2
-    })
-
-    // place bushes on the board
-    this.bushes.forEach((bush) => {
-      const bushCell = this.getRandomEmptyCell(CELL_TYPES[2].type)
-      bush.x = bushCell.element.x.baseVal.value + (CELL_SIZE - bush.width) / 2
-      bush.y = bushCell.element.y.baseVal.value + (CELL_SIZE - bush.height) / 2
-    })
-
     this.width = this.cells[0].length * CELL_SIZE
     this.height = this.cells.length * CELL_SIZE
+
+    svg.viewBox.baseVal.width = this.width
+    svg.viewBox.baseVal.height = this.height
   }
 }
 /*
