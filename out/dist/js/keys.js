578c68e01167a49dfb2184b77d95795e34010743
Maksim Mikhailov <max@mer.pw>
2023-02-15 20:48:33 +0200

    Fix capitalized keys bug

--- out/dist/js/keys.js
+++ out/dist/js/keys.js
@@ -14,15 +14,16 @@ const KeyState = {
 export default KeyState;
 // TODO restyle moves to WASD keys. And F and T keys for fung.
 window.addEventListener("keydown", (event) => {
-    if (event.key in KeyState) {
-        // TODO add a check for if no another keys are already pressed, to prevent diagonal movement
-        KeyState[event.key] = true;
+    const key = event.key.match(/^[A-Z]$/) ? event.key.toLowerCase() : event.key;
+    if (key in KeyState) {
+        KeyState[key] = true;
         mainHero.checkKeys();
     }
 });
 window.addEventListener("keyup", (event) => {
-    if (event.key in KeyState) {
-        KeyState[event.key] = false;
+    const key = event.key.match(/^[A-Z]$/) ? event.key.toLowerCase() : event.key;
+    if (key in KeyState) {
+        KeyState[key] = false;
         mainHero.checkKeys();
     }
 });
--- src/keys.ts
+++ src/keys.ts
@@ -17,16 +17,17 @@ export default KeyState
 // TODO restyle moves to WASD keys. And F and T keys for fung.
 
 window.addEventListener("keydown", (event: KeyboardEvent) => {
-  if (event.key in KeyState) {
-    // TODO add a check for if no another keys are already pressed, to prevent diagonal movement
-    KeyState[event.key as keyof typeof KeyState] = true
+  const key = event.key.match(/^[A-Z]$/) ? event.key.toLowerCase() : event.key
+  if (key in KeyState) {
+    KeyState[key as keyof typeof KeyState] = true
     mainHero.checkKeys()
   }
 })
 
 window.addEventListener("keyup", (event: KeyboardEvent) => {
-  if (event.key in KeyState) {
-    KeyState[event.key as keyof typeof KeyState] = false
+  const key = event.key.match(/^[A-Z]$/) ? event.key.toLowerCase() : event.key
+  if (key in KeyState) {
+    KeyState[key as keyof typeof KeyState] = false
     mainHero.checkKeys()
   }
 })
