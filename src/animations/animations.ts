d6add3e5e857cb1469fd11c54e4ed21f571abd43
healingdrawing <kyznector@gmail.com>
2023-02-23 01:14:56 +0200

    Add cloud animation data. The "pink" for a fungus cloud. The "blue" for a hero appearance.

--- src/animations/animations.ts
+++ src/animations/animations.ts
@@ -1,11 +1,13 @@
 import hero from "./hero.js"
 import sheep from "./sheep.js"
 import fungus from "./fungus.js"
+import cloud from "./cloud.js"
 
 const animations = {
   hero,
   sheep,
   fungus,
+  cloud,
 } as const
 
 export type AssetName = keyof typeof animations
---
+++ src/animations/cloud.ts
@@ -0,0 +1,20 @@
+import { getFrame } from "./frame"
+
+const frames = {
+  pinkBig: getFrame(3, 3),
+  pinkMiddle: getFrame(3, 4),
+  pinkSmall: getFrame(3, 5),
+  blueBig: getFrame(4, 5),
+  blueMiddle: getFrame(4, 6),
+  blueSmall: getFrame(4, 7),
+}
+
+export default {
+  pink: [
+    frames.pinkMiddle,
+    frames.pinkBig,
+    frames.pinkMiddle,
+    frames.pinkSmall,
+  ],
+  blue: [frames.blueBig, frames.blueMiddle, frames.blueSmall],
+} as const
