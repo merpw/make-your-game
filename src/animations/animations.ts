3f71f136ef9526c771e6968bbead9db6803f6e59
healingdrawing <kyznector@gmail.com>
2023-02-23 01:43:59 +0200

    Add portal animation data.

--- src/animations/animations.ts
+++ src/animations/animations.ts
@@ -2,12 +2,14 @@ import hero from "./hero.js"
 import sheep from "./sheep.js"
 import fungus from "./fungus.js"
 import cloud from "./cloud.js"
+import portal from "./portal.js"
 
 const animations = {
   hero,
   sheep,
   fungus,
   cloud,
+  portal,
 } as const
 
 export type AssetName = keyof typeof animations
---
+++ src/animations/portal.ts
@@ -0,0 +1,13 @@
+import { getFrame } from "./frame"
+
+const frames = {
+  stand0: getFrame(4, 0),
+  stand1: getFrame(4, 1),
+  stand2: getFrame(4, 2),
+  stand3: getFrame(4, 3),
+}
+
+export default {
+  off: [frames.stand0],
+  on: [frames.stand1, frames.stand2, frames.stand3],
+} as const
