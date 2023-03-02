095293098a9590edb7882c25ea145d21b3555844
natkim <natalia.kim.masseli@gmail.com>
2023-03-02 20:33:18 +0200

    add blueGiant cloud frame

--- src/animations/cloud.ts
+++ src/animations/cloud.ts
@@ -4,9 +4,10 @@ const frames = {
   pinkBig: getFrame(3, 3, 16, 16),
   pinkMiddle: getFrame(3, 4, 16, 16),
   pinkSmall: getFrame(3, 5, 16, 16),
-  blueBig: getFrame(5, 1, 16, 16),
-  blueMiddle: getFrame(5, 2, 16, 16),
-  blueSmall: getFrame(5, 3, 16, 16),
+  blueGiant: getFrame(5, 1, 16, 16),
+  blueBig: getFrame(5, 2, 16, 16),
+  blueMiddle: getFrame(5, 3, 16, 16),
+  blueSmall: getFrame(5, 4, 16, 16),
 }
 
 // TODO: maybe make cloud green to set any color using filters?
@@ -18,5 +19,5 @@ export default {
     frames.pinkMiddle,
     frames.pinkSmall,
   ],
-  blue: [frames.blueSmall, frames.blueMiddle, frames.blueBig],
+  blue: [frames.blueSmall, frames.blueMiddle, frames.blueBig, frames.blueGiant],
 } as const
