merpw/make-your-game

commit 2b300f247cc546e210f1064dd92107602c446c42

Offset for sheep path.

src/sheep.ts
@@ -91,34 +91,41 @@ export default class Sheep extends Creature<"sheep"> {
     switch (this.direction) {
       case "right":
         this.x += this.speed * frameTimeDiff
-        if (this.x >= this.targetCell.x) {
-          this.x = this.targetCell.x
+        if (this.x - this.dx >= this.targetCell.x) {
+          this.x = this.targetCell.x + this.dx
+          this.y = this.targetCell.y + this.dy
           this.targetCell = null
         }
         break
       case "bottom":
         this.y += this.speed * frameTimeDiff
-        if (this.y >= this.targetCell.y) {
-          this.y = this.targetCell.y
+        if (this.y - this.dy >= this.targetCell.y) {
+          this.y = this.targetCell.y + this.dy
+          this.x = this.targetCell.x + this.dx
           this.targetCell = null
         }
         break
       case "left":
         this.x -= this.speed * frameTimeDiff
-        if (this.x <= this.targetCell.x) {
-          this.x = this.targetCell.x
+        if (this.x - this.dx <= this.targetCell.x) {
+          this.x = this.targetCell.x + this.dx
+          this.y = this.targetCell.y + this.dy
           this.targetCell = null
         }
         break
       case "top":
         this.y -= this.speed * frameTimeDiff
-        if (this.y <= this.targetCell.y) {
-          this.y = this.targetCell.y
+        if (this.y - this.dy <= this.targetCell.y) {
+          this.y = this.targetCell.y + this.dy
+          this.x = this.targetCell.x + this.dx
           this.targetCell = null
         }
         break
     }
   }
+
+  private dx = 1.1
+  private dy = 1.5
 
   /**
    * Sets random {@link direction} and {@link targetCell} from one of the available directions
@@ -172,6 +179,8 @@ export default class Sheep extends Creature<"sheep"> {
 
   constructor(cell: Cell, neighbours: NeighbourCells, demonized = true) {
     super(SHEEP_SIZE, cell.x, cell.y, "sheep")
+    super.x += this.dx
+    super.y += this.dy
     this.genetics = 1 - GENETICS + Math.random() * GENETICS * 2
     // random value between 1 - GENETICS and 1 + GENETICS
 

commit 2923b4783f8c57e107a2e0cc9ec23c0e367cff64

Fix unexpected collisions with lucky Hero
prevention of exiting from the cell in Sheep was moved from
 render to set direction in 2b300f2, which caused unexpected overlaps
Restore it to make render more safe

src/sheep.ts
@@ -108,6 +108,7 @@ export default class Sheep extends Creature<"sheep"> {
       case "right": {
         this.x += this.speed * frameTimeDiff
         if (this.x >= this.targetCell.x) {
+          this.x = this.targetCell.x
           this.targetCell = null
         }
         break
@@ -115,6 +116,7 @@ export default class Sheep extends Creature<"sheep"> {
       case "bottom": {
         this.y += this.speed * frameTimeDiff
         if (this.y >= this.targetCell.y) {
+          this.y = this.targetCell.y
           this.targetCell = null
         }
         break
@@ -122,6 +124,7 @@ export default class Sheep extends Creature<"sheep"> {
       case "left": {
         this.x -= this.speed * frameTimeDiff
         if (this.x <= this.targetCell.x) {
+          this.x = this.targetCell.x
           this.targetCell = null
         }
         break
@@ -129,6 +132,7 @@ export default class Sheep extends Creature<"sheep"> {
       case "top": {
         this.y -= this.speed * frameTimeDiff
         if (this.y <= this.targetCell.y) {
+          this.y = this.targetCell.y
           this.targetCell = null
         }
         break